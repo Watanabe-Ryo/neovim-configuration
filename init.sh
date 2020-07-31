db2e4b80154e87674fd403b63e87ad730d8d6c8c
watanabe-ryo <hi.white.spot.rojer@gmail.com>
2020-07-31 23:36:54 +0900

    change condition: type to which

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir dein nvim_python_dev
 git clone https://github.com/Shougo/dein.vim.git dein/dein.vim
-if type "python3 -V" > /dev/null 2>&1
+if [ -x which "python3" ]
 then
     python3 -m venv nvim_python_dev/nvim
     source ./nvim_python_env/nvim/bin/activate
